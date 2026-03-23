530752285a7b3c59480314d5adb6f8bc21d79f3c
Shobhit9945 <shobhitbaijal5@gmail.com>
2026-03-24 01:09:42 +0530

    Revert "admin login fix attempt 2"
    This reverts commit ddf050ebf43a590bbd3f4a8e556377c42fcbfb6f.

--- server/index.ts
+++ server/index.ts
@@ -219,7 +219,6 @@ export async function createServer(): Promise<Express> {
     "https://www.docnearme.app",
     "https://clinic.docnearme.app",
     "https://www.clinic.docnearme.app",
-    "https://admin.docnearme.jp",
     "https://admin.docnearme.app",
     "https://clinics.docnearme.app",
   ].filter(Boolean) as string[];
